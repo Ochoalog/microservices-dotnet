aed44f777695f31224f025f11181f56dd496fcd8
Vitor Macedo Ochôa <vitor.ochoa@sequor.com.br>
2022-08-13 17:47:58 -0300

    feat: discount.API conteinerizada

--- src/Services/Discount/Discount.API/Repository/DiscountRepository.cs
+++ src/Services/Discount/Discount.API/Repository/DiscountRepository.cs
@@ -22,7 +22,7 @@ namespace Discount.API.Repository
                 (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
             var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
-                ("SELECT * FROM coupon WHERE ProductName = @ProductName", new { ProductName = productName});
+                ("SELECT * FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName});
 
             if(coupon is null)
                 return new Coupon() { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
@@ -36,7 +36,7 @@ namespace Discount.API.Repository
                 (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
             var affected = await connection.ExecuteAsync
-                ("INSERT INTO coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)", 
+                ("INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)", 
                 new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
 
             if (affected == 0)
@@ -51,7 +51,7 @@ namespace Discount.API.Repository
                 (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
             var affected = await connection.ExecuteAsync
-                ("UPDATE coupon SET ProductName = @ProductName, Description = @Description, @Amount = @Amount WHERE Id = @Id",
+                ("UPDATE Coupon SET ProductName = @ProductName, Description = @Description, @Amount = @Amount WHERE Id = @Id",
                 new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount, Id = coupon.Id });
 
             if (affected == 0)
@@ -66,7 +66,7 @@ namespace Discount.API.Repository
                (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
             var affected = await connection.ExecuteAsync
-                ("DELETE FROM coupon WHERE ProductName = @ProductName",
+                ("DELETE FROM Coupon WHERE ProductName = @ProductName",
                 new { ProductName = productName });
 
             if (affected == 0)
