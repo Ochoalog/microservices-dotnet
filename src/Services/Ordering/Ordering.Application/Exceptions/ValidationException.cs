c68621ac22f818176debb9c95f9eb55f8fe3b43a
Vitor Macedo Ochoa <vitor.ochoa@stone.com.br>
2023-01-21 18:53:57 -0300

    feat - adiciona delete e update order

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ordering.Application.Exceptions
+{
+    internal class ValidationException : ApplicationException
+    {
+        public IDictionary<string, string[]> Errors { get; }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+            : base("One or more validations failures have occurred.")
+        {
+            Errors = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+    }
+}
